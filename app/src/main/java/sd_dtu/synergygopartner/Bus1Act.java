27837745ae8b828d92e85f695bf24ff9fc329e40
sam AR <ashish05182001@gmail.com>
2016-08-16 18:02:17 +0530

    part2

--- app/src/main/java/sd_dtu/synergygopartner/Bus1Act.java
+++ app/src/main/java/sd_dtu/synergygopartner/Bus1Act.java
@@ -4,14 +4,55 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class Bus1Act extends AppCompatActivity {
+
+    EditText name,desig,contact,offTele,bussNature,YearCompany,noEmployee;
+    Spinner typeCompany;
+    ArrayAdapter<CharSequence> typecompadapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus_1);
+
+        name= (EditText)findViewById(R.id.nameeditText);
+        desig= (EditText)findViewById(R.id.designationeditText);
+        contact= (EditText)findViewById(R.id.phoneeditText);
+        offTele= (EditText)findViewById(R.id.offteleditText);
+        bussNature= (EditText)findViewById(R.id.busnatureeditText);
+        YearCompany= (EditText)findViewById(R.id.yearseditText);
+        noEmployee= (EditText)findViewById(R.id.EmpeditText);
+        typeCompany= (Spinner) findViewById(R.id.spinnecompanytyper);
+
+
+        typecompadapter = ArrayAdapter.createFromResource(this,R.array.company,R.layout.support_simple_spinner_dropdown_item);
+        typecompadapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        typeCompany.setAdapter(typecompadapter);
+
+        typeCompany.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
+
+
+
     public void onClickNextb1(View view){
 
         Intent intent=new Intent(Bus1Act.this,LocationPhoto.class);
